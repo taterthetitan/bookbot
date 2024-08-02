be32c6f52cfd24f1ff60b863325ecfe232712ace
taterthetitan <wwilkers@protonmail.com>
2024-08-01 20:44:02 -0400

    added report feature and added sorting to the dictionary

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text.lower())
     char_count = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
+    
 
-    print(char_count)
+    #print(char_count)
+
+    print("--- Begin book report on Frankenstein ---")
+    print(f"{num_words} words found in the document")
+    char_keys = list(char_count.keys())
+    char_values = list (char_count.values())
+    for i in range(len(char_keys)):
+        print(f"The character '{char_keys[i]}' was found {char_values[i]} times.")
+    print("--- End Report ---")
+
+
+
 
 
 def get_num_words(text):
@@ -25,8 +36,11 @@ def get_chars_dict(text):
             chars[lowered] += 1
         else:
             chars[lowered] = 1
-    return chars      
-
-
+    char_keys = list(chars.keys())
+    char_keys.sort()
+    sorted_chars = {i:chars[i] for i in char_keys}
+    
+    
+    return sorted_chars      
 
 main()
